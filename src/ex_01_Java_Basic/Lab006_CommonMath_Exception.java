bc21e3e39470a3edd89fe87648248c4a0cdb3155
chandulalmr <chandhulal.mr@gmail.com>
2025-01-09 23:31:33 +0530

    corrections - warning fix on xero throw error

--- src/ex_01_Java_Basic/Lab006_CommonMath_Exception.java
+++ src/ex_01_Java_Basic/Lab006_CommonMath_Exception.java
@@ -2,7 +2,7 @@ package ex_01_Java_Basic;
 
 public class Lab006_CommonMath_Exception {
     public static void main(String[] args) {
-        System.out.println(10/0);
+//        System.out.println(10/0);
         // by zero will throw error java.lang.ArithmeticException
     }
 }
