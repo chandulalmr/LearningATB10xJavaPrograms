aefd441b5b3820450bf1878f89de7a14a48f1576
chandulalmr <chandhulal.mr@gmail.com>
2025-01-09 23:05:28 +0530

    Initial commit

---
+++ src/ex_01_Java_Basic/Lab006_CommonMath_Exception.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab005_CommonMath_Exception {
+    public static void main(String[] args) {
+        System.out.println(10/0);
+        // by zero will throw error java.lang.ArithmeticException
+    }
+}
